c955f5255ed8a6c3c971b3c08e68d0ec8f9f8e23
Hary <azharimad@users.noreply.github.com>
2023-02-28 22:52:34 +0700

    Update and rename golanginstall.sh to initial-install.sh

--- initial-install.sh
+++ initial-install.sh
@@ -1,3 +1,5 @@
+sudo apt update && sudo apt upgrade -y && sudo apt install curl tar wget aria2 net-tools clang pkg-config libssl-dev jq build-essential bsdmainutils git make ncdu gcc git jq chrony liblz4-tool -y
+
 ver="1.19.6"
 cd $HOME
 wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
