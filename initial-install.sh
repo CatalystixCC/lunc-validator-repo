7af895997d9d314adba206a5a76819fe96869933
Hary <azharimad@users.noreply.github.com>
2023-02-28 23:14:15 +0700

    Update initial-install.sh

--- initial-install.sh
+++ initial-install.sh
@@ -6,9 +6,9 @@ wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
 rm "go$ver.linux-amd64.tar.gz"
-echo "export GOROOT=/usr/local/go" >> ~/.bash_profile
-echo "export GOPATH=$HOME/go" >> ~/.bash_profile
-echo "export GO111MODULE=on" >> ~/.bash_profile
-echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
-source ~/.bash_profile
+echo "export GOROOT=/usr/local/go" >> ~/.profile
+echo "export GOPATH=$HOME/go" >> ~/.profile
+echo "export GO111MODULE=on" >> ~/.profile
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.profile
+source ~/.profile
 go version
