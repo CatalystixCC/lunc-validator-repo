cd71b1b079518021c1496eabc3ec39be05acda7d
Hary <azharimad@users.noreply.github.com>
2024-02-19 21:13:14 +0700

    Update initial-install.sh

--- initial-install.sh
+++ initial-install.sh
@@ -1,6 +1,6 @@
 sudo apt update && sudo apt upgrade -y && sudo apt install curl tar wget aria2 net-tools clang pkg-config libssl-dev jq build-essential bsdmainutils git make ncdu gcc git jq chrony liblz4-tool -y
 
-ver="1.20.10"
+ver="1.20.14"
 cd $HOME
 wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
 sudo rm -rf /usr/local/go
